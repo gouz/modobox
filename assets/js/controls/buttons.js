9437804f137619ca4a1199dfebde324a1f2d4081
eliorivero <eliorivero@gmail.com>
2013-05-29 01:11:22 -0300

    close button

--- assets/js/controls/buttons.js
+++ assets/js/controls/buttons.js
@@ -8,7 +8,7 @@
 			if (typeof m.opts.c_buttons.next === "undefined")
 				m.opts.c_buttons.next = 'next item';
 			if (typeof m.opts.c_buttons.close === "undefined")
-				m.opts.c_buttons.close = 'close';
+				m.opts.c_buttons.close = '&times;';
 			var $controls = $('#modobox-controls', this.$inner);
 			$controls.append('<a id="modobox-prev" href="#">' + m.opts.c_buttons.prev + '</a><a id="modobox-next" href="#">' + m.opts.c_buttons.next + '</a><a id="modobox-close" href="#">' + m.opts.c_buttons.close + '</a>');
 			$('#modobox-prev', $controls).on('click', function () {
